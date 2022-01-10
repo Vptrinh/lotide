5b34837ad6f9dd486579edc214b9bf3d8620f895
Vivian Trinh <vptrinh@ryerson.ca>
2022-01-10 17:03:57 -0500

    head.js

--- head.js
+++ head.js
@@ -13,10 +13,10 @@ assertEqual(1,1);
 
 //FUNCTION TO RETURN THE FIRST ITEM IN THE ARRAY
 
-let head = function(array){
-  let firstItem = array[0]
+let head = function(array) {
+  let firstItem = array[0];
   return firstItem;
-}
+};
 
 //TEST CODE
 assertEqual(head([5,6,7]), 5);
