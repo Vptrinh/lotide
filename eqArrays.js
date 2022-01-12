f77412950558f2b9726cacfeb36875391f2536f9
Vivian Trinh <vptrinh@ryerson.ca>
2022-01-11 19:52:54 -0500

    Fixed mistakes so that it proves all indexes of the array are equal.

--- eqArrays.js
+++ eqArrays.js
@@ -9,19 +9,19 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) return false;
-  let answer = "";
+  let result = [];
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] === array2[i]) {
-      answer = true;
-    } else {
-      answer = false;
+      result.push(array1[i]);
     }
   }
-  return answer;
+  console.log("result:", result);
+  console.log("result:", array1);
+  return result.length === array1.length;
 };
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1]));// => false
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));// => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));// => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);// => false
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
