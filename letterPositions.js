c476413ec76fee3b46075a0198acbbcc7f6a44a7
Vivian Trinh <vptrinh@ryerson.ca>
2022-01-12 15:59:48 -0500

    A code to store the letter positions of a string into an object as an array.

---
+++ letterPositions.js
@@ -0,0 +1,35 @@
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) return false;
+  let result = [];
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      result.push(array1[i]);
+    }
+  }
+  return result.length === array1.length;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual,expected) === true) {
+    console.log(`${actual} === ${expected}`);
+  } else {
+    console.log(`${actual} !== ${expected}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i ++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+
+// TEST
+
+console.log(letterPositions("hello"));
