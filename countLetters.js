ad521a55b80a7739de614ca5942800a210f501e9
Vivian Trinh <vptrinh@ryerson.ca>
2022-01-12 15:28:25 -0500

    A function to count the letters in a string and store as an object.

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${actual} === ${expected}`);
+  } else {
+    console.log(`${actual} !== ${expected}`);
+  }
+};
+
+//FUNCTION IMPLEMENTATION
+
+const countLetters = function(string) {
+  let result = {};
+  for (let letters of string) {
+    if (result[letters]) {
+      result[letters] += 1;
+    } else {
+      result[letters] = 1;
+    }
+  }
+  return result;
+};
+
+// TEST CODE
+console.log(countLetters("This is a dog."));
