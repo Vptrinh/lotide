6fe8df435786c0ba3b507a4a5ed51b2639e18cef
Vivian Trinh <vptrinh@ryerson.ca>
2022-01-11 21:31:48 -0500

    Refactoring to remove additional console.log checks and add more edge control

--- without.js
+++ without.js
@@ -20,9 +20,7 @@ const assertArraysEqual = function(actual, expected) {
 const without = function(array1, array2) {
   let result = [];
   const length = array1.length > array2.length ? array1.length : array2.length;
-  console.log("length:", length);
   for (let i = 0; i < length; i++) {
-    console.log(!array1.includes(array2[i]));
     if (!array2.includes(array1[i]) && array1[i] !== undefined) {
       result.push(array1[i]);
     }
@@ -30,10 +28,10 @@ const without = function(array1, array2) {
   return result;
 };
 
+console.log(without([1, 2, 3, 5, "6"], [1, 2, 4, 6, "3"]));
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
 console.log(without([1, 2, 3], [1])); // => [2, 3]
-console.log(without(["1", "2", "3"], [5, 2, 1, 2, "3"])); // => ["1", "2"]);
-
-const words = ["lighthouse"];
-console.log(without(words, ["hello", "world", "lighthouse"])); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"])); 
+console.log(assertArraysEqual(words, ["hello", "world", "lighthouse"]));
