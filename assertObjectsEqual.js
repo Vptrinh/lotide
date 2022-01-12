e47b052e029f0882cf8a0fd01999a4564e4fed6b
Vivian Trinh <vptrinh@ryerson.ca>
2022-01-12 18:08:10 -0500

    A function to assert that two objects are equal to each other, utilizing the eqObjects function.

---
+++ assertObjectsEqual.js
@@ -0,0 +1,39 @@
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } else {
+    for (let someKeys in object1) {
+      if (object1[someKeys] !== object2[someKeys]) return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual,expected) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+
+const ab = {
+  a: 1,
+  b: 2,
+};
+
+const ba = {
+  b:2,
+  a:1,
+};
+
+const acb = {
+  a:1,
+  c:3,
+  b:2,
+};
+
+assertObjectsEqual(ab,ba);
+assertObjectsEqual(ab,acb);
