a46214d3606d894e4b9619291d6af44b36eeaf49
Vivian Trinh <vptrinh@ryerson.ca>
2022-01-12 15:29:19 -0500

    A function to compare an array to the true values in an object and count them.

---
+++ countOnly.js
@@ -0,0 +1,42 @@
+//assertEqual function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${actual} === ${expected}`);
+  } else {
+    console.log(`${actual} !== ${expected}`);
+  }
+};
+
+
+// FUNCTION IMPLEMENTATION
+const countOnly = function(allItems, itemsToCount) {
+  const results = {}
+  for (const item of allItems) {
+    if(itemsToCount[item]) {
+      if (results[item]){
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  console.log(results)
+  return results
+}
+    
+  
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
