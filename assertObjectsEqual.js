5da132e1a0d11b4e50612354a8a8bbdb3cddf702
Vivian Trinh <vptrinh@ryerson.ca>
2022-01-12 20:38:52 -0500

    Added eqArrays

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,3 +1,14 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) return false;
+  let result = [];
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      result.push(array1[i]);
+    }
+  }
+  return result.length === array1.length;
+};
+
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
