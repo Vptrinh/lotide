1a95ec55c020bc55a6409d053ad369c9b061581f
Vivian Trinh <vptrinh@ryerson.ca>
2022-01-13 17:42:29 -0500

    A function to find a key in an object using callbacks.

---
+++ findKey.js
@@ -0,0 +1,24 @@
+//Implementation
+
+const findKey = (object, callback) => {
+  let keys = Object.keys(object);
+  for (const key of keys) {
+    if (callback(object[key])) {
+      console.log(key);
+      return key;
+    }
+  }
+};
+
+
+findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+); // => "noma"
