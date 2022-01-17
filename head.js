d75494ebb67ff6268695e41252b7457cb1d1075d
Vivian Trinh <vptrinh@ryerson.ca>
2022-01-17 16:21:45 -0500

    Added mocha and chai

--- head.js
+++ head.js
@@ -9,6 +9,8 @@ let head = function(array) {
   return firstItem;
 };
 
-//TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
+
+// //TEST CODE
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
