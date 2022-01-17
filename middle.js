09270faf2d991ba9d6314d95fcbbfdcd79eec2cc
Vivian Trinh <vptrinh@ryerson.ca>
2022-01-17 16:15:25 -0500

    Refactored for modulization

--- middle.js
+++ middle.js
@@ -1,22 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) return false;
-  let result = [];
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      result.push(array1[i]);
-    }
-  }
-  return result.length === array1.length;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual,expected) === true) {
-    console.log(`${actual} === ${expected}`);
-  } else {
-    console.log(`${actual} !== ${expected}`);
-  }
-};
-
 const middle = function(array) {
   let result = [];
   let middleIndex = "";
@@ -36,10 +17,12 @@ const middle = function(array) {
   return result;
 };
 
+module.exports = middle;
+
 //TEST CODE
-middle([1]); // => []
-middle([1, 2]); // => []
-middle([1, 2, 3]); // => [2]
-middle([1, 2, 3, 4, 5]); // => [3]
-middle([1, 2, 3, 4]); // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+// middle([1]); // => []
+// middle([1, 2]); // => []
+// middle([1, 2, 3]); // => [2]
+// middle([1, 2, 3, 4, 5]); // => [3]
+// middle([1, 2, 3, 4]); // => [2, 3]
+// middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
