32559d76a6f0bd7fb3a910ee8266de90c939d53a
Vivian Trinh <vptrinh@ryerson.ca>
2022-01-17 12:43:04 -0500

    Edited files to be modularized

--- head.js
+++ head.js
@@ -1,15 +1,6 @@
 //FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`${actual} === ${expected}`);
-  } else {
-    console.log(`${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require ('./assertEqual');
 
-//TEST CODE
-assertEqual("Lighthouse Labs","Bootcamp");
-assertEqual(1,1);
 
 //FUNCTION TO RETURN THE FIRST ITEM IN THE ARRAY
 
